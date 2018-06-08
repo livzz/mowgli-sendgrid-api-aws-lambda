9107ae512d71d0b02f9c68286f356fe9e0310b6e
Alibh Deori <alibh619@gmail.com>
2018-06-08 13:58:26 +0530

    init Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const sendgrid = require("./sendgrid.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const msg = {
+    to: "alibh@righthemisphere.in",
+    from: "dont-reply@droidvue.com",
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>Yeeessss</strong>",
+    templateId: "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe",
+    substitutions: {
+        name: "Yaaaai"
+    }
+};
+
+app.post("/notify", async (req, res) => {
+    let response = {
+        error: "Sending email Failed!!",
+        message: null
+    };
+    msg.to = req.body.to;
+    try {
+        const result = await sendgrid.send(msg);
+        if (result) {
+            console.log("Success: ",result);
+            response.error = null;
+            response.message = result;
+        }
+    } catch (e) {
+        response.message = result;
+        console.log("Error: ",result);
+    }
+    res.json(response);
+});
+
+app.use("/", async (req, res) => {
+    res.send("You are not authorized!!");
+});
+
+module.exports.handler = serverless(app);
---
+++ sendgrid.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setSubstitutionWrappers("{{", "}}");
+exports.send = function (msg) {
+    return sgMail
+        .send(msg);
+};
