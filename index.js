b139390a89c1ae0c734ba572cceef080bba2cbc9
Alibh Deori <alibh619@gmail.com>
2018-06-13 12:16:48 +0530

    Refactored with seperate message class

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const serverless = require("serverless-http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const sendgrid = require("./sendgrid.js");
-const cors = require('cors')
+const messageBuilder = require("./message");
+const cors = require('cors');
 
 const app = express();
 
@@ -12,34 +13,22 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const msg = {
-    to: "alibh@righthemisphere.in",
-    from: "dont-reply@droidvue.com",
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>Yeeessss</strong>",
-    templateId: "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe",
-    substitutions: {
-        name: "Yaaaai"
-    }
-};
-
 app.post("/notify", async (req, res) => {
+    let msgBuilder = new messageBuilder(req.body);
     let response = {
         error: "Sending email Failed!!",
         message: null
     };
-    msg.to = req.body.to;
     try {
-        const result = await sendgrid.send(msg);
+        const result = await sendgrid.send(msgBuilder.getMessage());
         if (result) {
             console.log("Success: ",result);
             response.error = null;
             response.message = result;
         }
     } catch (e) {
-        response.message = result;
-        console.log("Error: ",result);
+        response.message = e;
+        console.log("Error: ",e);
     }
     res.json(response);
 });
