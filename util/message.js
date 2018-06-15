59be45e1cb858cbdd12f5deed506d3443ec9d55e
Alibh Deori <alibh619@gmail.com>
2018-06-15 18:38:48 +0530

    Added TODO

--- util/message.js
+++ util/message.js
@@ -3,7 +3,7 @@ class Message {
     constructor(request){
         this.msgToClient = {
             to: request.to,
-            from: "dont-reply@droidvue.com",
+            from: "dont-reply@droidvue.com", // TODO: Change domain name
             // subject: "Sending with SendGrid is Fun",
             // text: "and easy to do anywhere, even with Node.js",
             // html: "<strong>Yeeessss</strong>",
@@ -26,14 +26,14 @@ class Message {
                 this.msgToClient.subject = "Newsletter Subscription";
                 this.msgToClient.text = "You have been successfully registered to our newsletter.";
                 this.msgToClient.html = "<div style='text-align: center; color: cadetblue;'>You have been successfully registered to our newsletter.</div>";
-                this.msgToClient.templateId = "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe";
+                this.msgToClient.templateId = "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe"; // TODO: update Template ID
                 return this.msgToClient;
             // For Craft Acknowledgement
             case "1":
                 this.msgToClient.subject = "Acknowledgement";
                 this.msgToClient.text = "We have received your craft, will get back to you soon.";
                 this.msgToClient.html = "<div style='text-align: center; color: cadetblue;'>We have received your craft, will get back to you soon.</div>";
-                this.msgToClient.templateId = "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe";
+                this.msgToClient.templateId = "a09e0e42-eab7-4141-9142-3d6e1e2ff3fe"; // TODO: update Template ID
                 return this.msgToClient;
         }
     }
@@ -45,14 +45,14 @@ class Message {
                 this.msgToOwner.subject = "New Subscriber";
                 this.msgToOwner.text = "You have a new subscriber";
                 this.msgToOwner.html = "<div style='text-align: center; color: cadetblue;'>You have a new subscriber</div>";
-                this.msgToOwner.templateId = "566dbd06-2a9c-4668-813c-4aa12d52f465";
+                this.msgToOwner.templateId = "566dbd06-2a9c-4668-813c-4aa12d52f465"; // TODO: update Template ID
                 return this.msgToOwner;
             // For Craft Acknowledgement
             case "1":
                 this.msgToOwner.subject = "New Craft";
                 this.msgToOwner.text = "You have a new craft request";
                 this.msgToOwner.html = "<div style='text-align: center; color: cadetblue;'>You have a new craft request</div>";
-                this.msgToOwner.templateId = "566dbd06-2a9c-4668-813c-4aa12d52f465";
+                this.msgToOwner.templateId = "566dbd06-2a9c-4668-813c-4aa12d52f465"; // TODO: update Template ID
                 return this.msgToOwner;
         }
     }
